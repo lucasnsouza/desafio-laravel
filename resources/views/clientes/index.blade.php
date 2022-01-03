502e905d4746dc131af2e22b92d919347fdf2127
Lucas Souza <lucas_nevestj@hotmail.com>
2022-01-03 12:37:16 -0300

    responsive tables

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -40,44 +40,45 @@
         </div>
     </form>
 
-    <table class="table">
-        <thead>
-            <tr>
-            <th scope="col">Nome</th>
-            <th scope="col">Telefone</th>
-            <th scope="col">Email</th>
-            <th scope="col">Estado</th>
-            <th scope="col">Cidade</th>
-            <th scope="col">Situação</th>
-            <th scope="col"></th>
-            </tr>
-        </thead>
-        @foreach($clientes as $cliente)
-        <tbody>
-            <tr>
-            <th scope="row">{{$cliente->nome}}</th>
-            <td>{{$cliente->telefone}}</td>
-            <td>{{$cliente->email}}</td>
-            <td>{{$cliente->estado}}</td>
-            <td>{{$cliente->cidade}}</td>
-            <td>{{$cliente->situacao}}</td>
-            <td class="d-flex">
-                <a href="/clientes/{{$cliente->id}}/exibir" class="btn btn-info btn-sm mr-1">
-                    <i class="fas fa-external-link-alt"></i>
-                </a>
-
-                <form method="post" action="/clientes/remover/{{$cliente->id}}"
-                    onsubmit="return confirm('Tem certeza que deseja remover o cliente {{addslashes($cliente->nome)}}?')">
-                        @csrf
-                        <button class="btn btn-danger btn-sm">
-                            <i class="far fa-trash-alt"></i>
-                        </button>
-                </form>
-            </td>
-            </tr>
-        </tbody>
-        @endforeach
-    </table>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Telefone</th>
+                <th scope="col">Email</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Cidade</th>
+                <th scope="col">Situação</th>
+                <th scope="col"></th>
+                </tr>
+            </thead>
+            @foreach($clientes as $cliente)
+            <tbody>
+                <tr>
+                <th scope="row">{{$cliente->nome}}</th>
+                <td>{{$cliente->telefone}}</td>
+                <td>{{$cliente->email}}</td>
+                <td>{{$cliente->estado}}</td>
+                <td>{{$cliente->cidade}}</td>
+                <td>{{$cliente->situacao}}</td>
+                <td class="d-flex">
+                    <a href="/clientes/{{$cliente->id}}/exibir" class="btn btn-info btn-sm mr-1">
+                        <i class="fas fa-external-link-alt"></i>
+                    </a>
+                    <form method="post" action="/clientes/remover/{{$cliente->id}}"
+                        onsubmit="return confirm('Tem certeza que deseja remover o cliente {{addslashes($cliente->nome)}}?')">
+                            @csrf
+                            <button class="btn btn-danger btn-sm">
+                                <i class="far fa-trash-alt"></i>
+                            </button>
+                    </form>
+                </td>
+                </tr>
+            </tbody>
+            @endforeach
+        </table>
+    </div>
     <div class="d-flex justify-content-center">
         {{  $clientes->appends(["filtro" => isset($filtro) ? $filtro : " ", "coluna" => isset($coluna) ? $coluna : " "])->links() }}
     </div>
--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -39,44 +39,46 @@
         </div>
     </form>
 
-    <table class="table">
-        <thead>
-            <tr>
-            <th scope="col">Nome</th>
-            <th scope="col">Telefone</th>
-            <th scope="col">Email</th>
-            <th scope="col">Estado</th>
-            <th scope="col">Cidade</th>
-            <th scope="col">Situação</th>
-            <th scope="col"></th>
-            </tr>
-        </thead>
-        @foreach($users as $user)
-        <tbody>
-            <tr>
-            <th scope="row">{{$user->name}}</th>
-            <td>{{$user->phone}}</td>
-            <td>{{$user->email}}</td>
-            <td>{{$user->state}}</td>
-            <td>{{$user->city}}</td>
-            <td>{{$user->situation}}</td>
-            <td class="d-flex">
-                <a href="/usuarios/{{$user->id}}/exibir" class="btn btn-info btn-sm mr-1">
-                    <i class="fas fa-external-link-alt"></i>
-                </a>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Telefone</th>
+                <th scope="col">Email</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Cidade</th>
+                <th scope="col">Situação</th>
+                <th scope="col"></th>
+                </tr>
+            </thead>
+            @foreach($users as $user)
+            <tbody>
+                <tr>
+                <th scope="row">{{$user->name}}</th>
+                <td>{{$user->phone}}</td>
+                <td>{{$user->email}}</td>
+                <td>{{$user->state}}</td>
+                <td>{{$user->city}}</td>
+                <td>{{$user->situation}}</td>
+                <td class="d-flex">
+                    <a href="/usuarios/{{$user->id}}/exibir" class="btn btn-info btn-sm mr-1">
+                        <i class="fas fa-external-link-alt"></i>
+                    </a>
+                    <form method="post" action="/usuarios/remover/{{$user->id}}"
+                        onsubmit="return confirm('Tem certeza que deseja remover o cliente {{addslashes($user->name)}}?')">
+                            @csrf
+                            <button class="btn btn-danger btn-sm">
+                                <i class="far fa-trash-alt"></i>
+                            </button>
+                    </form>
+                </td>
+                </tr>
+            </tbody>
+            @endforeach
+        </table>
+    </div>
 
-                <form method="post" action="/usuarios/remover/{{$user->id}}"
-                    onsubmit="return confirm('Tem certeza que deseja remover o cliente {{addslashes($user->name)}}?')">
-                        @csrf
-                        <button class="btn btn-danger btn-sm">
-                            <i class="far fa-trash-alt"></i>
-                        </button>
-                </form>
-            </td>
-            </tr>
-        </tbody>
-        @endforeach
-    </table>
     <div class="d-flex justify-content-center">
     {{$users->appends(["filtro" => isset($filtro) ? $filtro : " ", "coluna" => isset($coluna)? $coluna : " "])->links()}}
     </div>
